aa19848151eccbd939ad8e3174b8a3c7db5e067a
sometann <114172513+sometann@users.noreply.github.com>
2022-09-27 11:15:57 +0300

    Delete task1(15.09.2022).py

--- task1(15.09.2022).py
+++
@@ -1,9 +0,0 @@
-meal = input ("Прием пищи:")
-if meal == 'Завтрак':
-    print ('Каша')
-else:
-     wish = input('Предпочтение')
-     if wish == "Плотно поесть":
-         print('Плов')
-     else:
-        print('Котлета с пюре')
